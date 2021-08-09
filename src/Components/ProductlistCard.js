607e42f82d18fd550796e82d6885a6f36bc9ef72
Shreya Gupta <shreyagupta@Shreya-Guptas-MacBook-Pro.local>
2021-08-09 11:46:17 +0530

    update

---
+++ src/Components/ProductlistCard.js
@@ -0,0 +1,25 @@
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { Component } from 'react'
+
+export default class ProductlistCard extends Component {
+  render() {
+    return (
+        <div className="overflow-hidden shadow-lg border border-gray-200 bg-white mx-3 my-0 w-256">
+        <img className="w-full hover:bg-grey-200" style={{height:"225px"}} src={this.props.image} alt={this.props.brand}/>
+        <div className="p-2 px-3">
+          <div className="font-semibold text-lg mb-2 line-clamp-1">{this.props.brand}</div>
+          <div className="font-light text-gray-500 text-xs  mb-2 line-clamp-2">{this.props.desc}</div>
+          <div className="font-bold text-gray-500 text-md mb-2 ">Rs.{this.props.price}</div>
+          <button className="font-semibold text-pink-600 w-full text-sm border border-pink-600 py-1 mb-1 uppercase">
+              Add to wishlist  <span className="text-xl ml-1"><FontAwesomeIcon icon={faHeart}/></span>
+          </button>
+          <button className="font-semibold bg-pink-600 text-white w-full text-sm border border-pink-600 py-1 mb-1 uppercase">
+              Add to Bag <span className="text-xl ml-1"><FontAwesomeIcon icon={faShoppingBag}/></span>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
--- src/ProductList/Main.js
+++ src/ProductList/Main.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react'
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
-
+import ProductlistCard from '../Components/ProductlistCard'
+import Axios from "axios";
+import {FixedSizeGrid, FixedSizeList} from 'react-window'
 export default class ProductList extends Component {
-  render() {
+    state = {
+        mydata:[],
+    };
+    componentDidMount(){
+        Axios.get(`https://api.jsonbin.io/b/610ba3f2e1b0604017a6fbcc`)
+        .then(
+            res => {
+                this.setState({mydata : res.data});
+            }
+        )
+        .catch(
+            err => console.log(err)
+        )
+    }
+    
+    render() {
+        const myprod = ({rowIndex,columnIndex,style}) => {
+        const product = this.state.mydata[rowIndex+columnIndex] || {}
+            return(
+            <div style={style}>
+            <ProductlistCard key={product["SERIAL NO"]} image={product.IMAGE} brand={product.NAME} desc={product["DESCRIPTION & COLOR"]} price={product.PRICE}/>
+            </div>
+            )
+        };
+
+      console.log(this.state.mydata)
     return (
       <>
         <Navbar/>
         <div className="bg-green-200" style={{height:"200px"}}></div>
-        <div className="grid bg-blue-200 grid-cols-4"style={{height:"1000px"}}>
+        
+        <div className="grid grid-cols-4">
             {/* filter section */}
-        <div></div>
+        <div className="bg-blue-200 border">
+            fliter
+        </div>
+
+
         {/* productlist */}
-        <div className="bg-red-200 col-span-3"></div>
+        <div className="col-span-3 flex justify-center py-4 border-l border-t`">
+        {/* <div className="grid grid-cols-5 p-2" style={{border:"1px solid red"}}>
+
+        {this.state.mydata.map(product => (
+            <ProductlistCard key={product["SERIAL NO"]} image={product.IMAGE} brand={product.NAME} desc={product["DESCRIPTION & COLOR"]} price={product.PRICE} />
+        ))}
+            </div> */}
+        <FixedSizeGrid 
+        columnCount={1} 
+        rowCount={this.state.mydata.length/5} 
+        height={1000} 
+        width={1300} 
+        rowHeight={500} 
+        columnWidth={260}>
+            {myprod}
+        </FixedSizeGrid>
+
+        </div>
+
         </div>
         <Footer/>
       </>
